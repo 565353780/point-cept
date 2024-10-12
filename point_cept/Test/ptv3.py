10abaf4cf50c40baa9e4cff632aa7cd2b008283e
chLi <565353780@qq.com>
2024-10-12 23:04:59 +0800

    add batch support

--- point_cept/Test/ptv3.py
+++ point_cept/Test/ptv3.py
@@ -5,12 +5,12 @@ from pointcept.models.point_transformer_v3 import PointTransformerV3
 
 
 def test():
-    points = torch.rand((400, 3), dtype=torch.float32, device='cuda')
+    points = torch.rand((2, 400, 3), dtype=torch.float32, device='cuda')
 
     data = {
-        'coord': points,
-        'feat': points,
-        'batch': torch.zeros(points.shape[0], dtype=torch.long, device='cuda'),
+        'coord': points.reshape(-1, 3),
+        'feat': points.reshape(-1, 3),
+        'batch': torch.cat([torch.ones(points.shape[1], dtype=torch.long, device='cuda') * i for i in range(points.shape[0])]),
         'grid_size': 0.01,
     }
 
@@ -18,5 +18,7 @@ def test():
 
     point = ptv3(data)
 
-    print('point.feat.shape: ', point.feat.shape)
+    feature = point.feat.reshape(points.shape[0], points.shape[1], -1)
+
+    print('point feature shape: ', feature.shape)
     return True
