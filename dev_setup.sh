0d747655cede8a9ff0dbfc058ddb70a075054767
chLi <565353780@qq.com>
2026-02-27 15:49:56 +0800

    fix pip install sparsehash bug

--- dev_setup.sh
+++ dev_setup.sh
@@ -8,9 +8,9 @@ pip install -U cython
 pip install torch==2.5.0 torchvision==0.20.0 torchaudio==2.5.0 \
   --index-url https://download.pytorch.org/whl/cu124
 
-pip install -U packaging ninja sparsehash h5py pyyaml sharedarray tensorboard \
-  tensorboardx wandb yapf addict einops scipy plyfile termcolor timm ftfy regex \
-  tqdm matplotlib black open3d
+pip install -U packaging ninja h5py pyyaml sharedarray tensorboard \
+  tensorboardx wandb yapf addict einops scipy plyfile termcolor \
+  timm ftfy regex tqdm matplotlib black open3d
 
 pip install torch-cluster torch-scatter torch-sparse \
   -f https://data.pyg.org/whl/torch-2.5.0+cu124.html
@@ -18,6 +18,8 @@ pip install torch-cluster torch-scatter torch-sparse \
 pip install -U torch-geometric
 
 pip install -U spconv-cu124
+
+pip install sparsehash --no-build-isolation
 
 #pip install -U git+https://github.com/octree-nn/ocnn-pytorch.git
 #pip install -U git+https://github.com/openai/CLIP.git
