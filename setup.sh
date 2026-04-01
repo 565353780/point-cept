14cd539414f809c0a0446363af66a72313a0a22f
chLi <565353780@qq.com>
2026-04-01 13:16:22 +0800

    update setup bash files

--- setup.sh
+++ setup.sh
@@ -3,26 +3,26 @@ git clone --depth 1 --recursive -b v2.8.3 https://github.com/Dao-AILab/flash-att
 
 conda install -c conda-forge sparsehash -y
 
-pip install -U cython
+pip install cython
 
 pip install torch torchvision torchaudio \
   --index-url https://download.pytorch.org/whl/cu124
 
-pip install -U packaging ninja h5py pyyaml sharedarray tensorboard \
+pip install packaging ninja h5py pyyaml sharedarray tensorboard \
   tensorboardx wandb yapf addict einops scipy plyfile termcolor \
   timm ftfy regex tqdm matplotlib black open3d
 
 pip install torch-cluster torch-scatter torch-sparse \
   -f https://data.pyg.org/whl/torch-2.6.0+cu124.html
 
-pip install -U torch-geometric
+pip install torch-geometric
 
-pip install -U spconv-cu124
+pip install spconv-cu124
 
 pip install sparsehash --no-build-isolation
 
-#pip install -U git+https://github.com/octree-nn/ocnn-pytorch.git
-#pip install -U git+https://github.com/openai/CLIP.git
+#pip install git+https://github.com/octree-nn/ocnn-pytorch.git
+#pip install git+https://github.com/openai/CLIP.git
 cd flash-attention
 python setup.py install
 
